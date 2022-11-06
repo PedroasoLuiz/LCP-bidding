e2dd99d5bcf6e6681ca65bf99009449d64d06596
Camila Teodoro <81452961+omillaa@users.noreply.github.com>
2022-11-06 17:31:17 -0300

    repositorio

---
+++ Projeto01/Model/IRepositorio.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto01.Model
+{
+    internal interface IRepositorio<T>
+    {
+        void  Add(T obj);
+        void  Update(T obj);
+        void  Delete(T obj);
+    }
+}
