a5a2aa6314b46e94076332ccb721e9fcda4bf82e
laripsilva25 <117595456+laripsilva25@users.noreply.github.com>
2022-11-06 19:12:48 -0300

    RepositorioProduto
    RepositorioProduto + Add IdProduto na classe Produto

--- Projeto01/Model/Produto.cs
+++ Projeto01/Model/Produto.cs
@@ -13,11 +13,13 @@ namespace Projeto01.Model
         private double custo;
         private string unidade;
         private int fkMarcas;
+        private int idProduto;
 
         public int CodProduto { get => codProduto; set => codProduto = value; }
         public string Descricao { get => descricao; set => descricao = value; }
         public double Custo { get => custo; set => custo = value; }
         public string Unidade { get => unidade; set => unidade = value; }
         public int FkMarcas { get => fkMarcas; set => fkMarcas = value; }
+        public int IdProduto { get => idProduto; set => idProduto = value; }
     }
 }
